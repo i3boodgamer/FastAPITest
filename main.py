9696f6a11b8d0831c4d7941a8a42ce7655b7381b
Богдан <91775355+i3boodgamer@users.noreply.github.com>
2024-08-05 12:11:32 +0700

    Не создаются таблицы при каждом запуске

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import uvicorn
 from api_v1 import router as router_v1
 
 from contextlib import asynccontextmanager
-from core.models import Base, db_helper
 from core.config import settings
 
 from typing import Annotated
@@ -20,8 +19,6 @@ from users.views import router as users_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with db_helper.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
         
     yield
     
